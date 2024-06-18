403c860e4506a9654c8b864f9f0d09bfaa158021
kudinov <s.kudinov@list.ru>
2024-06-18 17:05:21 +0300

    tips add selectbox

--- pages/tips.py
+++ pages/tips.py
@@ -65,99 +65,114 @@ if tips is not None:
         st.download_button(label=f"Скачать график: {title}", data=buf, file_name=f"{filename}.png", mime="image/png")
         st.divider()
 
-    # График 1: Динамика чаевых во времени
-    st.subheader('Динамика чаевых во времени')
-    create_plot(
-        title='Динамика чаевых',
-        xlabel='Date',
-        ylabel='Tip %',
-        plot_func=sns.lineplot,
-        filename='tip_dynamics',
-        data=tips, x='date_only', y='tip %'
-    )
+    plot = st.selectbox('График', ['Динамика чаевых во времени',
+                            'Распределение чеков',
+                            'Связь между чеком и суммой чаевых',
+                            'Распределение чаевых по дням недели',
+                            'Распределение суммы чека по дням недели и приемам пищи',
+                            'Распределение чаевых на обед и ланч',
+                            'Распределение чаевых по полу и курению'])
 
-    # График 2: Распределение чеков
-    st.subheader('Распределение чеков')
-    create_plot(
-        title='Распределение чеков',
-        xlabel='',
-        ylabel='Сумма',
-        plot_func=sns.histplot,
-        filename='bill_distribution',
-        data=tips, x='total_bill'
-    )
+    if plot == 'Динамика чаевых во времени':
+        # График 1: Динамика чаевых во времени
+        st.subheader('Динамика чаевых во времени')
+        create_plot(
+            title='Динамика чаевых',
+            xlabel='Date',
+            ylabel='Tip %',
+            plot_func=sns.lineplot,
+            filename='tip_dynamics',
+            data=tips, x='date_only', y='tip %'
+        )
 
-    # График 3: Связь между чеком и суммой чаевых
-    st.subheader('Связь между чеком и суммой чаевых')
-    create_plot(
-        title='Зависимость чаевых от чека',
-        xlabel='Сумма чека',
-        ylabel='Чаевые',
-        plot_func=sns.scatterplot,
-        filename='tips_vs_bill',
-        data=tips, x='total_bill', y='tip', size='tip %', sizes=(10, 100), legend=False
-    )
+    elif plot == 'Распределение чеков':
+        # График 2: Распределение чеков
+        st.subheader('Распределение чеков')
+        create_plot(
+            title='Распределение чеков',
+            xlabel='',
+            ylabel='Сумма',
+            plot_func=sns.histplot,
+            filename='bill_distribution',
+            data=tips, x='total_bill'
+        )
+
+    elif plot == 'Связь между чеком и суммой чаевых':
+        # График 3: Связь между чеком и суммой чаевых
+        st.subheader('Связь между чеком и суммой чаевых')
+        create_plot(
+            title='Зависимость чаевых от чека',
+            xlabel='Сумма чека',
+            ylabel='Чаевые',
+            plot_func=sns.scatterplot,
+            filename='tips_vs_bill',
+            data=tips, x='total_bill', y='tip', size='tip %', sizes=(10, 100), legend=False
+        )
 
     # График 4: Распределение чаевых по дням недели
-    st.subheader('Распределение чаевых по дням недели')
-    tips['day_of_week'] = tips['time_order'].dt.day_name()
-    days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    tips['day_of_week'] = pd.Categorical(tips['day_of_week'], categories=days_order, ordered=True)
-    create_plot(
-        title='Распределение % чаевых по дням недели',
-        xlabel='% чаевых',
-        ylabel='',
-        plot_func=sns.scatterplot,
-        filename='tips_by_weekday',
-        data=tips, y='day_of_week', x='tip %', hue='sex', palette={'Female': 'r', 'Male': 'b'}
-    )
+    elif plot == 'Распределение чаевых по дням недели':
+        st.subheader('Распределение чаевых по дням недели')
+        tips['day_of_week'] = tips['time_order'].dt.day_name()
+        days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        tips['day_of_week'] = pd.Categorical(tips['day_of_week'], categories=days_order, ordered=True)
+        create_plot(
+            title='Распределение % чаевых по дням недели',
+            xlabel='% чаевых',
+            ylabel='',
+            plot_func=sns.scatterplot,
+            filename='tips_by_weekday',
+            data=tips, y='day_of_week', x='tip %', hue='sex', palette={'Female': 'r', 'Male': 'b'}
+        )
 
-    # График 5: Распределение суммы чека по дням недели и приемам пищи
-    st.subheader('Распределение суммы чека по дням недели и приемам пищи')
-    tips['time_only'] = tips['time_order'].dt.time
+    elif plot == 'Распределение суммы чека по дням недели и приемам пищи':
+        # График 5: Распределение суммы чека по дням недели и приемам пищи
+        st.subheader('Распределение суммы чека по дням недели и приемам пищи')
+        tips['time_only'] = tips['time_order'].dt.time
 
-    def time_classificator(time):
-        if 9 <= time.hour < 12:
-            return "breakfast"
-        elif 12 <= time.hour < 16:
-            return "lunch"
-        elif 16 <= time.hour < 23:
-            return "dinner"
+        def time_classificator(time):
+            if 9 <= time.hour < 12:
+                return "breakfast"
+            elif 12 <= time.hour < 16:
+                return "lunch"
+            elif 16 <= time.hour < 23:
+                return "dinner"
 
-    tips['time_class'] = tips['time_only'].apply(time_classificator)
-    create_plot(
-        title='Счета по дням недели',
-        xlabel='',
-        ylabel='Сумма чека',
-        plot_func=sns.boxplot,
-        filename='bills_by_weekday',
-        data=tips, x='day_of_week', y='total_bill', hue='time_class', hue_order=['breakfast', 'lunch', 'dinner']
-    )
+        tips['time_class'] = tips['time_only'].apply(time_classificator)
+        create_plot(
+            title='Счета по дням недели',
+            xlabel='',
+            ylabel='Сумма чека',
+            plot_func=sns.boxplot,
+            filename='bills_by_weekday',
+            data=tips, x='day_of_week', y='total_bill', hue='time_class', hue_order=['breakfast', 'lunch', 'dinner']
+        )
 
     # График 6: Распределение чаевых на обед и ланч
-    st.subheader('Распределение чаевых на обед и ланч')
-    tips_lunch = tips[tips['time_class'] == 'lunch']
-    tips_dinner = tips[tips['time_class'] == 'dinner']
+    elif plot == 'Распределение чаевых на обед и ланч':
+        st.subheader('Распределение чаевых на обед и ланч')
+        tips_lunch = tips[tips['time_class'] == 'lunch']
+        tips_dinner = tips[tips['time_class'] == 'dinner']
 
-    fig_lunch = px.histogram(tips_lunch, x='tip %', title='Распределение чаевых во время ланча')
-    fig_dinner = px.histogram(tips_dinner, x='tip %', title='Распределение чаевых во время обеда')
+        fig_lunch = px.histogram(tips_lunch, x='tip %', title='Распределение чаевых во время ланча')
+        fig_dinner = px.histogram(tips_dinner, x='tip %', title='Распределение чаевых во время обеда')
 
-    fig_lunch.update_layout(xaxis_title='% чаевых', yaxis_title='')
-    fig_dinner.update_layout(xaxis_title='% чаевых', yaxis_title='')
+        fig_lunch.update_layout(xaxis_title='% чаевых', yaxis_title='')
+        fig_dinner.update_layout(xaxis_title='% чаевых', yaxis_title='')
 
-    st.plotly_chart(fig_lunch)
-    st.plotly_chart(fig_dinner)
-    st.divider()
+        st.plotly_chart(fig_lunch)
+        st.plotly_chart(fig_dinner)
+        st.divider()
 
     # График 7: Распределение чаевых по полу и курению
-    st.subheader('Распределение чаевых по полу и курению')
-    fig_male = px.scatter(tips[tips['sex'] == 'Male'], x='total_bill', y='tip', color='smoker',
-                          title='Чаевые от мужчин', labels={'total_bill': 'Чек', 'tip': 'Чаевые'})
-    fig_female = px.scatter(tips[tips['sex'] == 'Female'], x='total_bill', y='tip', color='smoker',
-                            title='Чаевые от женщин', labels={'total_bill': 'Чек', 'tip': 'Чаевые'})
+    elif plot == 'Распределение чаевых по полу и курению':
+        st.subheader('Распределение чаевых по полу и курению')
+        fig_male = px.scatter(tips[tips['sex'] == 'Male'], x='total_bill', y='tip', color='smoker',
+                              title='Чаевые от мужчин', labels={'total_bill': 'Чек', 'tip': 'Чаевые'})
+        fig_female = px.scatter(tips[tips['sex'] == 'Female'], x='total_bill', y='tip', color='smoker',
+                                title='Чаевые от женщин', labels={'total_bill': 'Чек', 'tip': 'Чаевые'})
 
-    st.plotly_chart(fig_male)
-    st.plotly_chart(fig_female)
+        st.plotly_chart(fig_male)
+        st.plotly_chart(fig_female)
 
 else:
     st.subheader("Загрузите датафрейм для продолжения.")
